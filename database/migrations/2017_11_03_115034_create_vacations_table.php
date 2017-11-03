dc71887e0c60c111f10bc46b6c1079d0b341597e
Driton Berisha <tonnb9@gmail.com>
2017-11-03 13:24:41 +0100

    Init models, migrations & seeds

---
+++ database/migrations/2017_11_03_115034_create_vacations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVacationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vacations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('from_date');
+            $table->date('to_date');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->boolean('approved');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vacations');
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Driton Berisha',
+            'email' => 'dritonnaserberisha@gmail.com',
+            'password' => bcrypt('123456'),
+        ]);
+    }
+}
